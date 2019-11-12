1ba361a2de1721104a8d90616ef89153f7a043cb
samuel <aarnelhernandez@gmail.com>
2019-11-12 09:32:01 -0600

    first commit

---
+++ src/components/NavComponents/Categories/Processors.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Processors extends Component {
+    render() {
+        return (
+            <div>
+                These are processors
+            </div>
+        )
+    }
+}
+
+export default Processors
